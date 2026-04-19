e872b36361e1e4dfb22484ebe50834a18e75385e
Cupcak3p3nguin <jtran060517@gmail.com>
2026-04-19 16:34:47 -0700

    Update time_ui.gd

--- minigame-5/scripts/time_ui.gd
+++ minigame-5/scripts/time_ui.gd
@@ -4,7 +4,11 @@ extends VBoxContainer
 @onready var conesText = get_node("ConesText")
 
 var time: float = 0.0
-
+var cones_hit = 0
 func _process(delta: float) -> void:
 	time += delta
 	text.text = "time: %.2f" % time
+
+func _increaseConesHit():
+	cones_hit += 1
+	$ConesText.text = "Cones Hit: " + str(cones_hit)
